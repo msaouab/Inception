d984675fe2aa1859df76acd3bdcf90bfae6674b0
msaouab <saouab.mohammed@gmail.com>
2022-10-23 18:09:15 +0100

    move to goinfre imac

---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+mkdir -p /var/www/html/wordpress
+wp core download --allow-root --path=/var/www/html/wordpress
+
+# sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+tail -f /dev/null
