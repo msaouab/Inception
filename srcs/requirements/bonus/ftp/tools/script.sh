4680ac44d36fb6c1113ef466c5be0910955751ff
msaouab <saouab.mohammed@gmail.com>
2022-10-31 22:54:46 +0100

    i need to keep ftp container running

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -1,22 +1,12 @@
 #!/bin/sh
 
-# adduser "$FTP_USER"
-
-# chown nobody:nogroup /home/"$FTP_USER"/ftp
-# mkdir -p /home/"$FTP_USER"/ftp/files
-# chmod a-w /home/"$FTP_USER"/ftp
-# chown "$FTP_USER":"$FTP_USER" /home/med/ftp/files
+useradd -m "$FTP_USER"
+echo "$FTP_USER:$FTP_PASS" | chpasswd
+mkdir -p /var/run/vsftpd/empty/
+echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
 
 /etc/init.d/vsftpd start
 
-# exec "$@"
+# /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
 
-/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
-
-# useradd -m "$FTP_USER"
-# echo "$FTP_USER:$FTP_PASS" | chpasswd
-# mkdir -p /home/"$FTP_USER"/
-# mkdir -p /var/run/vsftpd/empty/
-# echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
-
-# exec "$@"
+exec "$@"
