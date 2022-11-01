982157c71a09e23fe36f8f677293c6839a822d4e
msaouab <saouab.mohammed@gmail.com>
2022-11-01 13:59:46 +0100

    last version in FTP

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -5,8 +5,4 @@ echo "$FTP_USER:$FTP_PASS" | chpasswd
 mkdir -p /var/run/vsftpd/empty/
 echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
 
-# /etc/init.d/vsftpd start
-
-# /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
-
 exec "$@"
