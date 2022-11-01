bc2f889543a1ffb0b45aeb06ded58af63f293dec
msaouab <saouab.mohammed@gmail.com>
2022-11-01 12:46:09 +0100

    ftp done

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -5,7 +5,7 @@ echo "$FTP_USER:$FTP_PASS" | chpasswd
 mkdir -p /var/run/vsftpd/empty/
 echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
 
-/etc/init.d/vsftpd start
+# /etc/init.d/vsftpd start
 
 # /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
 
